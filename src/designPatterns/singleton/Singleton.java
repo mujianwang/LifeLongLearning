39eb43de84c6cd15d2cdc3e2fa7144bdaf2159d1
mujianwang <jianwang@eastmoney.com>
2020-07-17 15:55:54 +0800

    单例模式

---
+++ src/designPatterns/singleton/Singleton.java
@@ -0,0 +1,44 @@
+package designPatterns.singleton;
+
+/**
+ * @program: LifeLongLearning
+ * @description 单例模式示例
+ * @author: mujianwang
+ * @create: 2020-07-17 15:27
+ **/
+public class Singleton {
+
+    /**
+     * 静态实例
+     * JVM创建对象: 1.分配内存  2.初始化构造器  3.将对象指向分配的内存地址
+     * volatile关键字, 防止JVM指令重排序优化后, 2和3顺序交换, 返回一个实际未完成初始化的实例, 也保证实例状态对所有线程同步可见
+     */
+    private static volatile Singleton singleton;
+
+    /**
+     * 构造函数私有化
+     */
+    private Singleton() {
+    }
+
+    /**
+     * 对外发布实例获取方法(双重检查锁)
+     * 防止多线程下产生多个实例
+     *
+     * @return
+     */
+    public static Singleton getInstance() {
+
+        //第一次检查
+        if (singleton == null) {
+            synchronized (Singleton.class) {
+                //第二次检查
+                if (singleton == null) {
+                    singleton = new Singleton();
+                }
+            }
+        }
+        return singleton;
+    }
+
+}
---
+++ src/designPatterns/singleton/ThreadPoolUtil.java
@@ -0,0 +1,55 @@
+package designPatterns.singleton;
+
+import java.util.concurrent.*;
+
+/**
+ * @program: LifeLongLearning
+ * @description 单例线程池工具类
+ * @author: mujianwang
+ * @create: 2020-07-17 15:50
+ **/
+public class ThreadPoolUtil {
+
+    /**
+     * 线程池实例
+     */
+    private static volatile ExecutorService executor;
+
+    /**
+     * 单例定制化创建线程池
+     *
+     * @return
+     */
+    private static ExecutorService getExecutor() {
+        if (executor == null) {
+            synchronized (ExecutorService.class) {
+                if (executor == null) {
+                    executor = new ThreadPoolExecutor(10, 20,
+                            6000L, TimeUnit.SECONDS,
+                            new LinkedBlockingQueue<>(1000), new ThreadPoolExecutor.CallerRunsPolicy());
+                }
+            }
+        }
+        return executor;
+    }
+
+    /**
+     * 异步执行任务(无返回值)
+     *
+     * @param thread
+     */
+    public static void addTask(Runnable thread) {
+        getExecutor().submit(thread);
+    }
+
+    /**
+     * 异步执行任务(有返回值)
+     *
+     * @param task
+     * @param <V>
+     * @return
+     */
+    public static <V> Future addTask(Callable<V> task) {
+        return getExecutor().submit(task);
+    }
+}
